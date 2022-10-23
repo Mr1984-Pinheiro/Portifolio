dc52ffa724318b1f39677aea801c9298fdd53008
Eduardo <cadutapinheiro@gmail.com>
2022-10-23 02:24:35 -0300

    Soft Skills e menu fecha ao clicar

--- static/script.js
+++ static/script.js
@@ -6,6 +6,19 @@ const body = document.querySelector('body')
 menuMobile.addEventListener('click', () =>{
     menuMobile.classList.contains("bi-list") 
     ? menuMobile.classList.replace("bi-list", "bi-x")
-    : menuMobile.classList.remove("bi-x", "bi-list");
+    : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active");
+});
+
+/* Fecha menu quando clicado no item e abre list mobile */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
 });
