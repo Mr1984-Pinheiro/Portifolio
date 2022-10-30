f75410d8b7973ff1cc7b41f7852da137c4fcfe7f
Eduardo <cadutapinheiro@gmail.com>
2022-10-30 04:51:06 -0300

    Animação scroll

--- static/script.js
+++ static/script.js
@@ -22,3 +22,25 @@ navItem.forEach(item => {
         }
     })
 });
+
+// Animar todos os items na tela que tiverem meu atributo data-anime
+
+const item = document.querySelectorAll('[data-anime]');
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+    item.forEach(element => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animete")
+        } else {
+            element.classList.remove("animete");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener('scroll', () => {
+    animeScroll();
+})
