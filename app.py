4da124df24c82ec78dc493c3e7d85d44b150d971
Eduardo <cadutapinheiro@gmail.com>
2022-10-31 03:22:41 -0300

    Email do py configurado

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from socket import if_nameindex
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash
 from flask_mail import Mail, Message
 from config import email, senha
 
@@ -15,9 +15,45 @@ mail_settings = {
     "MAIL_PASSWORD": senha
 }
 
+app.config.update(mail_settings)
+mail = Mail(app)
+
+class Contato: 
+    def __init__(self, nome, email, mensagem):
+        self.nome = nome,
+        self.email = email,
+        self.mensagem = mensagem
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+        if request.method == 'POST':
+            formContato = Contato(
+                request.form["nome"],
+                request.form["email"],
+                request.form["mensagem"],
+
+            ) 
+
+            msg = Message(
+                subject = f'{formContato.nome} te enviou uma mensagem no portfólio',
+                sender = app.config.get("MAIL_USERNAME"),
+                recipients= ['obpccarlos@gmail.com', app.config.get("MAIL_USERNAME")],
+                body= f'''
+
+                {formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte mensagem:
+
+                {formContato.mensagem}
+                
+                '''
+            )
+            mail.send(msg)
+            flash('Mensagem enviada com sucesso!')
+        return redirect('/')
+
 
 if __name__ == '__main__': 
     app.run(debug=True)
