535a8bcce8c1fd5cbb9a2d3996a22646938b62b4
Eduardo <cadutapinheiro@gmail.com>
2022-10-31 02:01:32 -0300

    Animation and responsive sections

--- static/script.js
+++ static/script.js
@@ -25,16 +25,17 @@ navItem.forEach(item => {
 
 // Animar todos os items na tela que tiverem meu atributo data-anime
 
-const item = document.querySelectorAll('[data-anime]');
+const item = document.querySelectorAll("[data-anime]");
 
 const animeScroll = () => {
+
     const windowTop = window.pageYOffset + window.innerHeight * 0.85;
 
     item.forEach(element => {
         if (windowTop > element.offsetTop) {
-            element.classList.add("animete")
+            element.classList.add("animate")
         } else {
-            element.classList.remove("animete");
+            element.classList.remove("animate");
         }
     });
 };
